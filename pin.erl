e8181d9be574b8f01bc3d16c2b874c81b6a7ae3d
dusten <dusten@usa.net>
2012-06-19 00:31:16 -0400

    More Testing

--- pin.erl
+++ pin.erl
@@ -1,7 +1,7 @@
 -module(pin).
--export([read_gpio/1, readgpio/1, bin_to_hex/1, hex_to_bin/1]).
+-export([read_gpio/1, readgpio/1, bin_to_hex/1, hex_to_bin/1, save_current_state/1, ext_mode/1 ]).
 
-read_gpio(File) ->
+readgpio(File) ->
   case file:read_file(File) of
     {ok, Res} ->
       io:format(" ~n File Name: ~p~n Results: ~p~n",[File,Res]);
@@ -9,21 +9,60 @@ read_gpio(File) ->
       io:format(" Error read_file() : ~p~n",[Reason])
   end.
 
-readgpio(File) ->
-  case file:open(File, [read, binary, raw]) of
+read_gpio(File) ->
+  Results = case file:open(File, [read, binary, raw]) of
     {ok, Fd} ->
-      case file:pread(Fd, 47, 4) of
+      Result = case file:pread(Fd, 0, 512) of
         eof ->
-          io:format(" Opended the file but reached EOF unexpetedly.~n");
-        {error, Reason} ->
-          io:format(" Error file:pread() : ~p~n",[Reason]);
-	{ok, Res} -> 
-          io:format(" Results of file: ~p~n",[Res])
+	  {error, "Opended the file but reached EOF unexpetedly."};
+%%          io:format(" Opended the file but reached EOF unexpetedly.~n"); %% Needs Change for Debugging
+        {error, Res} ->
+          Tmp = io_lib:format(" Error file:pread(~p) : ~p~n",[File,Res]),
+	  {error, Tmp};
+        {ok, Res} -> 
+          {ok, Res}
       end,
-      file:close(Fd);
-    {error, Reason} ->
-      io:format(" Error file:open() : ~p~n",[Reason])
+      file:close(Fd),
+      Result;
+    {error, Res} ->
+      Tmp = io_lib:format(" Error file:open(~p) : ~p~n",[File,Res]),
+      {error, Tmp}
+  end,
+Results.
+
+
+ext_mode([]) -> string:join(["0","0","0"],"");
+ext_mode([Pin_Slew]) ->
+  if Pin_Slew == 0 ; Pin_Slew == 1 -> string:join([erlang:integer_to_list(Pin_Slew),"0","0"],"");    
+    true -> {error,"Invalid Ext_Mode Value Pin_Slew"}
+  end;
+ext_mode([Pin_Slew,Pin_UpDown]) ->
+  if Pin_Slew == 0 ; Pin_Slew == 1,
+     Pin_UpDown == 0 ; Pin_UpDown == 1 -> string:join([erlang:integer_to_list(Pin_Slew),erlang:integer_to_list(Pin_UpDown),"0"],"");
+    true -> {error,"Invalid Ext_Mode Value Pin_Slew or Pin_UpDown"}
+  end;
+ext_mode([Pin_Slew,Pin_UpDown,Pin_UpDown_E]) ->
+  if Pin_Slew == 0 ; Pin_Slew == 1,
+     Pin_UpDown == 0 ; Pin_UpDown == 1,
+     Pin_UpDown_E == 0 ; Pin_UpDown_E == 1 -> string:join([erlang:integer_to_list(Pin_Slew),erlang:integer_to_list(Pin_UpDown),erlang:integer_to_list(Pin_UpDown_E)],"");  
+    true -> {error,"Invalid Ext_Mode Value Pin_Slew or Pin_UpDown or Pin_UpDown_E"}
   end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%      Can use re:split in place of Token         %%
+%% re:split("Erlang","[ln]",[{return,list},trim]). %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+save_current_state(File) ->
+  {ok,Raw_File} = read_gpio(File),
+  [_Name,Reg,_Other] = string:tokens(binary:bin_to_list(Raw_File),"()"),
+  [_Other_Reg,Cur_State] = string:tokens(Reg,erlang:atom_to_list(' = ')),    
+  [_,Cur] = string:tokens(Cur_State,"x"),
+  S = erlang:bit_size(erlang:list_to_binary(Cur)),
+  <<Current_State:S/integer-little>>=erlang:list_to_binary(Cur),
+  Current_State.
+
 
 
 hex(N) when N < 10 -> $0+N;
